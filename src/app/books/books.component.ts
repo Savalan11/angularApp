6ebca42aaa49d4f8c760399f4c757a8fd5348b6c
savalan.suleymanli@gmail.com <savalan.suleymanli@gmail.com>
2022-12-23 22:58:44 +0100

    interpolation component added
    interpolation component added in books.components.html and ts. files

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -7,13 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-name = "Clean Code";
-author = "Robert Martin";
+name: string = "Pro Angular";
+author: string = "Adam Freeman";
+src: string = "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41r7+TqeYsL._SY344_BO1,204,203,200_.jpg";
+
+name2: string = "Advanced Guides to Master C#";
+author2: string = "Mark Reed";
+src2: string = "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41tKWuKfTsL._SX348_BO1,204,203,200_.jpg";
+
 
   constructor () { }
 
   ngOnInit(): void {
 
-  }
+}
 
 }
