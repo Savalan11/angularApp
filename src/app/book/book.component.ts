0723a8b854c43b1b1c26ad8e675d2b3691755d54
savalan.suleymanli@gmail.com <savalan.suleymanli@gmail.com>
2022-12-25 22:04:57 +0100

    types component created
    types component created for single page information

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from '../types/Book';
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent {
+
+export class BookComponent implements OnInit {
+
+  @Input() book: Book = {} as Book;
+  constructor () { }
+
+  ngOnInit(): void {
+
+  }
 
 }
+
