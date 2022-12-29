ca4798560e4ec908bd2ce8bcfefa9f029fde55ec
savalan.suleymanli@gmail.com <savalan.suleymanli@gmail.com>
2022-12-29 12:07:19 +0100

    Amendments in books list
    In books component the list of books have been updated

--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -18,24 +18,52 @@ export class BooksService {
         amount: 15,
       },
       {
-        name: "Advanced Guides to Master C#",
-        author: "Mark Reed",
-        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41tKWuKfTsL._SX348_BO1,204,203,200_.jpg",
-        amount: 25,
+        name: "Spring Boot, Angular",
+        author: "D. Basilian",
+        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51gvWmhCpZL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+        amount: 28,
     
       },
       {
-        name: "Advanced Guides to Master C#",
-        author: "Mark Reed",
-        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41tKWuKfTsL._SX348_BO1,204,203,200_.jpg",
-        amount: 25,
+        name: "Learning Angular",
+        author: "A. Bampakos",
+        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41yavYgXumL._SX404_BO1,204,203,200_.jpg",
+        amount: 56,
     
       },
       {
-        name: "Advanced Guides to Master C#",
-        author: "Mark Reed",
-        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41tKWuKfTsL._SX348_BO1,204,203,200_.jpg",
-        amount: 25,
+        name: "Angular Cookbook",
+        author: "Ahsan Ayaz",
+        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/411QTHyJ12L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+        amount: 17.99,
+    
+      },
+      {
+        name: "Beginning Angular",
+        author: "Greg Lim",
+        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41POWmbj-9L.jpg",
+        amount: 9.99,
+    
+      },
+      {
+        name: "Angular Router",
+        author: "Viktor Savkin",
+        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51roQ4-7XsL.jpg",
+        amount: 17,
+    
+      },
+      {
+        name: "ASP NET Core, Angular",
+        author: "De Santics",
+        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41wVJH+vU9L._SX258_BO1,204,203,200_.jpg",
+        amount: 37.99,
+    
+      },
+      {
+        name: "Essential Typescript",
+        author: "A. Freeman",
+        image: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41iL6ZZPBUL._SX348_BO1,204,203,200_.jpg",
+        amount: 51.51,
     
       },
     
